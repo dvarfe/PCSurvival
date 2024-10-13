b61c2186b527cf125e76ffd5adf879f50a9d6681
dvarfe <skt3032@yandex.ru>
2024-10-13 12:54:10 +0300

    Expanded information collected

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from posix import login_tty
 import psutil
 import time
 import signal
@@ -8,38 +9,54 @@ import csv
 # recorded on hard drive once in NUM_GET_STATS_ITERS * GET_STATS_TIMEOUT
 #RECORD_TIMEOUT = 15
 NUM_GET_STATS_ITERS = 5
-GET_STATS_TIMEOUT = 1
+GET_STATS_TIMEOUT = 2
+FILE_PATH = "data.csv"
 
-cpu_stats_keys = [["timestamp", "cpu_usage"]]
-ram_stats_keys = [["timestamp", "memory_usage"]]
-disk_stats_keys = [["timestamp", "disk_usage"]]
+DISK_PATH = '/'
 
-CPU_FILE_PATH = "cpu.csv"
-RAM_FILE_PATH = "ram.csv"
-DISK_FILE_PATH = "disk.csv"
 
-data = [[], [], []]
+csv_keys = [["timestamp", "device", "measure", "value"]]
+cpu_general_info = {"logical_CPU" : psutil.cpu_count(), 
+                    "physical_CPU" : psutil.cpu_count(logical = False)}
+ram_general_info = {"total_memory" : psutil.virtual_memory().total,
+                    "total_swap" : psutil.swap_memory().total}
+disks_general_info = {"num_partitions" : len(psutil.disk_partitions(all=True)),
+                      "physical_partitions" : len(psutil.disk_partitions(all=False))}
 
-def initialize_one_file(path, keys):
-    with open(path, mode='w', newline='') as file:
+data = []
+
+def initialize_data():
+    with open(FILE_PATH, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerows(keys) 
+        writer.writerows(csv_keys)
+        writer.writerows(get_general_info("cpu", cpu_general_info))
+        writer.writerows(get_general_info("ram", ram_general_info))
+        writer.writerows(get_general_info("disks", disks_general_info))
+        writer.writerows(get_temp_general_info())
 
-def initialize_files():
-    initialize_one_file(CPU_FILE_PATH, cpu_stats_keys)
-    initialize_one_file(RAM_FILE_PATH, ram_stats_keys)
-    initialize_one_file(DISK_FILE_PATH, disk_stats_keys)
+def get_general_info(device, meaurements):
+    gen_info = []
+    for key in meaurements:
+        cur_dict = [0, device, key, meaurements[key]]
+        gen_info.append(cur_dict)
+    return gen_info
 
-def save_one_file(path, data):
-    with open(path, "a") as csvfile:
-        print('writing data')
-        writer = csv.writer(csvfile)
-        writer.writerows(data)
+def get_temp_general_info():
+    info = []
+    sensors = psutil.sensors_temperatures()
+    for name, entries in sensors.items():
+        for entry in entries:
+            info.append([0, 'tempertature', 'critical_' + name + '|' + entry.label, entry.critical])
+    return info
+
+
 
 def save_data():
-    save_one_file(CPU_FILE_PATH, data[0])
-    save_one_file(RAM_FILE_PATH, data[1])
-    save_one_file(DISK_FILE_PATH, data[2])
+    with open(FILE_PATH, "a") as csvfile:
+        print('writing data')
+        writer = csv.writer(csvfile)
+        for line in data:
+            writer.writerows(line)
 
 def signal_handler(sig, frame):
     save_data()
@@ -49,23 +66,65 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
+def get_cpu_info():
+    cur_time = int(time.time())
+    cpu_percent = psutil.cpu_percent()
+    cpu_time = psutil.cpu_times()
+    cpu_freq = psutil.cpu_freq().current
+    measurement_dict = {"cpu_percent" : cpu_percent, "user_time" : cpu_time.user,
+                        "system_time" : cpu_time.system, "idle_time" : cpu_time.idle, 
+                        "frequency" : cpu_freq}
+    info = []   
+    for key in measurement_dict:
+        info.append([cur_time, "cpu", key, measurement_dict[key]])
+    return info
 
+def get_ram_info():
+    cur_time = int(time.time())
+    ram_percent = psutil.virtual_memory().percent
+    swap_percent = psutil.swap_memory().percent
+    measurement_dict = {"ram_percent" : ram_percent, "swap_percent" : swap_percent}
+    info = []
+    for key in measurement_dict:
+        info.append([cur_time, "ram", key, measurement_dict[key]])
+    return info
+
+def get_disk_info():
+    cur_time = int(time.time())
+    disk_percent = psutil.disk_usage(DISK_PATH).percent
+    disk_io = psutil.disk_io_counters()
+    measurement_dict = {"disk_percent" : disk_percent, "write_count" : disk_io.write_count,
+                        "write_bytes" : disk_io.write_bytes, "read_count" : disk_io.read_count,
+                        "read_bytes" : disk_io.read_bytes}
+    info = []
+    for key in measurement_dict:
+        info.append([cur_time, "disks", key, measurement_dict[key]])
+    return info
+
+def get_temperature():
+    cur_time = int(time.time())
+    sensors = psutil.sensors_temperatures()
+    info = []
+    for name, entries in sensors.items():
+        for entry in entries:
+            info.append([cur_time, 'tempertature', name + '|' + entry.label, entry.current])
+    return info
 def collect_stats():
     i = 1
     global data
 
     while True:
-        data[0].append([int(time.time()), psutil.cpu_percent()])
-        data[1].append([int(time.time()), psutil.virtual_memory().percent])
-        data[2].append([int(time.time()), psutil.disk_usage("/").percent])
-       
+        data.append(get_cpu_info())
+        data.append(get_ram_info())
+        data.append(get_disk_info())
+        data.append(get_temperature())
         if i == NUM_GET_STATS_ITERS:
             i = 0
             save_data()
-            data = [[], [], []]
+            data = []
         i += 1
         time.sleep(GET_STATS_TIMEOUT)
 if __name__ == "__main__":
-    initialize_files()
+    initialize_data()
     collect_stats()
 
